f4011cfd6a0572b482b087e823d247fe74d65866
wenchen1114 <54914849+wenchen1114@users.noreply.github.com>
2020-03-23 16:43:20 -0400

    all

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,4 @@
 $(document).ready(function(){
-$(".right_container__content").on("mouseover",function(){
-    $(".right_container__content").not(this).css("visibility","hidden");
-  });
-  $(".right_container__content").on("mouseout",function(){
-    $(".right_container__content").css("visibility","visible");
-  });
   //refernce by https://stackoverflow.com/questions/26694385/fade-in-on-scroll-down-fade-out-on-scroll-up-based-on-element-position-in-win
   $(window).scroll(function() {
     var windowBottom = $(this).scrollTop() + $(this).innerHeight();
@@ -20,6 +14,78 @@ $(".right_container__content").on("mouseover",function(){
       }
     });
   }).scroll(); //invoke scroll-handler on page-load
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 0){
+      $('.left_content__box1').fadeIn();
+    }
+    else {
+      $('.left_content__box1').fadeOut();
+    }
+  });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 505){
+      $('.left_content__box2').fadeIn();
+    }
+    else {
+      $('.left_content__box2').fadeOut();
+    }
+  });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 901){
+      $('.left_content__box3').fadeIn();
+    }
+    else {
+      $('.left_content__box3').fadeOut();
+    }
+  });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 1430){
+      $('.left_content__box4').fadeIn();
+    }
+    else {
+      $('.left_content__box4').fadeOut();
+    }
+  });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 2049){
+      $('.left_content__box5').fadeIn();
+    }
+    else {
+      $('.left_content__box5').fadeOut();
+    }
+  });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 2512){
+      $('.left_content__box6').fadeIn();
+    }
+    else {
+      $('.left_content__box6').fadeOut();
+    }
+  });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 3065){
+      $('.left_content__box7').fadeIn();
+    }
+    else {
+      $('.left_content__box7').fadeOut();
+    }
+  });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 4204){
+      $('.left_content__box8').fadeIn();
+    }
+    else {
+      $('.left_content__box8').fadeOut();
+    }
+  });
 
   $('.left_content__signup___button').click(async() => {
       fetch("/api/v1/signups",
